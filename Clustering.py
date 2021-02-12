5dd1f472a2bd064d507d17eb12810ce510b26d40
ThorChadMerlin <merlinlindsay@hotmail.com>
2021-02-12 16:02:45 +0000

    completed plotting

--- Clustering.py
+++ Clustering.py
@@ -1,8 +1,14 @@
 import numpy as np
 import pandas as pd
+
 from sklearn.cluster import KMeans
+from sklearn.metrics.pairwise import euclidean_distances
+
 import matplotlib.pyplot as plt
+from matplotlib import cm
+
 import itertools
+
 
 def table(df):
     tabl = {}
@@ -16,40 +22,54 @@ def table(df):
 def kmeans(df, k=3):
     X = df.values
     # split each x in X into 3 clusters
-    clusters = KMeans(n_clusters=k,random_state=0).fit_predict(X)
+    fitter = KMeans(n_clusters=k,random_state=0).fit(X)
+    clusters = fitter.predict(X)
     # return array of corresponding clusters
-    return clusters
+    return fitter, clusters
     
 
 
-def plotKmeans(X, clusters):
+def plotKmeans(X, clusters, k):
 
     pairs = [pair for pair in itertools.combinations(X.columns,2)]
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    y = clusters
-
-    n = 0
     for pair in pairs:
-        ax = fig.add_subplot(n+1,1,n+1)
+
+        colours = iter(cm.rainbow(np.linspace(0, 1, k)))
 
         Xsample = X[[ pair[0] , pair[1]]]
         for i in np.unique(clusters):
             c = "Cluster:" + str(i+1)
-            plt.scatter(Xsample.iloc[y==i,0],Xsample.iloc[y==i,1], c='red',label=c)
-        # plt.scatter(Xsample.iloc[y==0,0],Xsample.iloc[y==0,1], c='red',label='Cluster 1')
-        # plt.scatter(Xsample.iloc[y==1,0],Xsample.iloc[y==1,1], c='green',label='Cluster 2')
-        # plt.scatter(Xsample.iloc[y==2,0],Xsample.iloc[y==2,1], c='cyan',label='Cluster 3')
-        plt.show()
+            plt.scatter(Xsample.iloc[clusters==i,0],Xsample.iloc[clusters==i,1], color=next(colours),label=c)
+
+        name = (pair[0] + " " + pair[1] + ".png")
+        plt.savefig(name)
+        plt.clf()
+
+
+# INITIALISATION
 
 customerData = pd.read_csv('./data/wholesale_customers.csv',dtype=float)
 
 customerData.drop(["Channel","Region"],axis=1, inplace=True)
 
+
+# Part 1
 tabl = table(customerData)
 for attr in tabl:
     print(attr, " : ", tabl[attr])
 
-clusters = kmeans(customerData)
-plotKmeans(customerData,clusters)
+
+# Part 2
+k=3
+fitter, clusters = kmeans(customerData,k)
+plotKmeans(customerData,clusters,k)
+
+# Part 3
+kSet = [3,5,10]
+
+for i in kSet:
+    fitter, clusters = kmeans(customerData,i)
+    BC = euclidean_distances(fitter.cluster_centers_) 
+    WC
+    ratio = WC/BC
